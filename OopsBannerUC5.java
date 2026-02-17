046a088a20a9503cb561f63df64e746947cc4bd4
vk0483-create <vk0483@srmist.edu.in>
2026-02-17 16:01:18 +0530

    Create OopsBannerUC5.java

---
+++ OopsBannerUC5.java
@@ -0,0 +1,18 @@
+public class OopsBannerUC5 {
+    public static void main(String[] args) {
+
+        String[] banner = {
+            String.join("", " *****    ", " *****    ", " *****   ", "****** "),
+            String.join("", "*     *  ", "*     *  ", "*     *  ", "*     *"),
+            String.join("", "*     *  ", "*     *  ", "*     *  ", "*     *"),
+            String.join("", "*     *  ", "*     *  ", "******   ", "****** "),
+            String.join("", "*     *  ", "*     *  ", "*        ", "*"),
+            String.join("", "*     *  ", "*     *  ", "*        ", "*"),
+            String.join("", " *****    ", " *****   ", "*        ", "*")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
