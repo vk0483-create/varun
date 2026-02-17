ff6d8b764b4a7f8ca89dcd1133b38fdd33318118
vk0483-create <vk0483@srmist.edu.in>
2026-02-17 15:36:22 +0530

    Create OppsBannerUC3.java

---
+++ OppsBannerUC3.java
@@ -0,0 +1,25 @@
+public class OopsBannerUC3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",
+                " *****    ", " *****    ", " *****   ", "****** "));
+
+        System.out.println(String.join("",
+                "*     *  ", "*     *  ", "*     *  ", "*     *"));
+
+        System.out.println(String.join("",
+                "*     *  ", "*     *  ", "*     *  ", "*     *"));
+
+        System.out.println(String.join("",
+                "*     *  ", "*     *  ", "******   ", "****** "));
+
+        System.out.println(String.join("",
+                "*     *  ", "*     *  ", "*        ", "*"));
+
+        System.out.println(String.join("",
+                "*     *  ", "*     *  ", "*        ", "*"));
+
+        System.out.println(String.join("",
+                " *****    ", " *****   ", "*        ", "*"));
+    }
+}
